432794886a366432b260cf138651acb9b4a4c9e3
Sean Baildon <sean@sbaildon.com>
2014-08-15 15:55:35 +0100

    Allow the bootstrapper to be executed anywhere
    No longer do you have to be inside mevdds/scripts/ to execute start.sh, it should now be more relative

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,9 @@
 set -o nounset
 set -o errexit
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
 if [ $1 == 'opencache' ]; then
    bash 'generic/opencache/setup.sh'
 elif [ $1 == 'tools' ]; then
