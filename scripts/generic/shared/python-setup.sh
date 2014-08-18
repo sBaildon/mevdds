aaa3bd3368f26054d124c46b8a3a5cde9780f59d
David Butterworth <d.butterworth@lancaster.ac.uk>
2014-08-18 12:54:19 +0100

    Fixed python-setup.sh in scripts

--- scripts/generic/shared/python-setup.sh
+++ scripts/generic/shared/python-setup.sh
@@ -2,6 +2,6 @@
 apt-get update
 
 # Get everything we need from repositories
-apt-get -y install build-essential libsqlite3-dev zlib1g-dev libncurses5-dev libgdbm-dev libbz2-dev libreadline5-dev libssl-dev libdb-dev curl python-dev git vim unzip
+apt-get -y install build-essential libsqlite3-dev zlib1g-dev libncurses5-dev libgdbm-dev libbz2-dev libreadline-gplv2-dev libssl-dev libdb-dev curl python-dev git vim unzip
 
 cd ~
