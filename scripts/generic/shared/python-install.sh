4d3441ac8a169821a915ac5a5a5487243583f4cf
Sean Baildon <sean@sbaildon.com>
2014-08-21 09:33:28 +0100

    Check for absence of python 2.7
    Instead of checking if we have 2.6, check if we *don't* have 2.7, because 3.0+ also causes issues

--- scripts/generic/shared/python-install.sh
+++ scripts/generic/shared/python-install.sh
@@ -1,7 +1,7 @@
 echo "Python-setup"
 
 #Get Python 2.7 if necessary
-if [[ $(python --version 2>&1) == *2\.6.* ]]
+if [[ $(python --version 2>&1) != *2\.7.* ]]
 then
     echo "Python version is not compatible with OpenCache, installing 2.7"
     wget -P ~ http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tgz --no-check-certificate
