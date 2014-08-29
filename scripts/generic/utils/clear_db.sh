31cc83a262bb243fd8ec8d105d81048fe6aca792
Sean Baildon <sean@sbaildon.com>
2014-08-29 14:42:35 +0100

    Fix an error with bash OR
    Logical OR was broken, now fixed

--- scripts/generic/utils/clear_db.sh
+++ scripts/generic/utils/clear_db.sh
@@ -1,6 +1,6 @@
 database=$1
 
-if [ $1 == 'cont'  || $1 == 'controller']; then
+if [ $1 == 'cont' ] || [ $1 == 'controller' ]; then
     mongo opencache --eval "db.dropDatabase()"
 elif [ $1 == 'node' ]; then
   mongo opencache_node --eval "db.dropDatabase()"
