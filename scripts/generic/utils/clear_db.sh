9ca0de6dab73623a1be39ce875ad4662a1d4065f
Sean Baildon <sean@sbaildon.com>
2014-08-29 14:40:09 +0100

    Script to delete OpenCache database
    A simple script that can be used to quickly restart the OpenCache databse

---
+++ scripts/generic/utils/clear_db.sh
@@ -0,0 +1,7 @@
+database=$1
+
+if [ $1 == 'cont'  || $1 == 'controller']; then
+    mongo opencache --eval "db.dropDatabase()"
+elif [ $1 == 'node' ]; then
+  mongo opencache_node --eval "db.dropDatabase()"
+fi
