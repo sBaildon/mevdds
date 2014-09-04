d87bd6f1d170d006ab233d871f37a24d304a11e4
David Butterworth <d.butterworth@lancaster.ac.uk>
2014-09-04 17:35:21 +0100

    Maybe fixed i2catVLAN.sh

--- scripts/specific/i2catVLAN.sh
+++ scripts/specific/i2catVLAN.sh
@@ -3,10 +3,15 @@
 VLAN=$1
 IP=$2
 
-ifconfig eth2 up
+#ifconfig eth2 up
 
-ifconfig add eth2 $VLAN
+#ifconfig add eth2 $VLAN
 
-ifconfig eth2.$VLAN $IP/24 up
+#ifconfig eth2.$VLAN $IP/24 up
 
 #ifconfig  eth2.$VLAN mtu 1496
+
+
+ip link add link eth2 name eth2.$VLAN type vlan $VLAN
+
+ip addr add $IP/24 dev eth2.$VLAN
