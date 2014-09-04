73c94f10d69456baab673c176fd004c29d081099
David Butterworth <d.butterworth@lancaster.ac.uk>
2014-09-04 17:46:58 +0100

    Fixed i2catVLAN.sh (hopefully for good this time)

--- scripts/specific/i2catVLAN.sh
+++ scripts/specific/i2catVLAN.sh
@@ -3,8 +3,8 @@
 VLAN=$1
 IP=$2
 
-apt-get install -y vlan
-
+apt-get install -y vlan # vlan isn't always installed..
+modprobe 8021q # need to load this module for vlan support
 
 ifconfig eth2 up
 
