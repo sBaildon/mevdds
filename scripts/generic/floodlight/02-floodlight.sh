3c7562fd79c5656074eee35d1402b3fa196e73a4
Sean Baildon <sean@sbaildon.com>
2014-09-05 15:07:38 +0100

    Changed floodlight version

--- scripts/generic/floodlight/02-floodlight.sh
+++ scripts/generic/floodlight/02-floodlight.sh
@@ -11,13 +11,13 @@ apt-get install -y ant unzip >> /var/log/mevdds/floodlight.log
 
 ## wget zip file
 echo "wget floodlight v0.90"
-wget https://github.com/floodlight/floodlight/archive/v0.90.zip -a /var/log/mevdds/floodlight.log
+wget https://github.com/sBaildon/floodlight/archive/master.zip -a /var/log/mevdds/floodlight.log
 
 ## unzip floodlight
 echo "[unzip floodlight]" | tee -a /var/log/mevdds/floodlight.log
-unzip v0.90.zip >> /var/log/mevdds/floodlight.log
+unzip master.zip >> /var/log/mevdds/floodlight.log
 
-cd floodlight-0.90
+cd floodlight
 
 ## compile floodlight
 echo "[Compililing with ant]" | tee -a /var/log/mevdds/floodlight.log
