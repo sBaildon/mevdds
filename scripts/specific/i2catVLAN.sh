85b05792d109e9e51cfabe62fff9c41495ad4e23
David Butterworth <d.butterworth@lancaster.ac.uk>
2014-09-10 11:47:17 +0100

    Fixed the problem with the OF for i2cat. The Docs were wrong..

--- scripts/specific/i2catVLAN.sh
+++ scripts/specific/i2catVLAN.sh
@@ -6,10 +6,10 @@ IP=$2
 apt-get install -y vlan # vlan isn't always installed..
 modprobe 8021q # need to load this module for vlan support
 
-ifconfig eth2 up
+ifconfig eth1 up
 
-vconfig add eth2 $VLAN
+vconfig add eth1 $VLAN
 
-ifconfig eth2.$VLAN $IP/24 up
+ifconfig eth1.$VLAN $IP/24 up
 
-ifconfig  eth2.$VLAN mtu 1496
+ifconfig  eth1.$VLAN mtu 1496
