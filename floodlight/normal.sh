41ff83ad6d2aefc54a9d4a5ba5f77b10623d43ee
Matthew Broadbent <matt@matthewbroadbent.net>
2014-09-10 14:39:15 +0100

    Fixed DPID passong

--- floodlight/normal.sh
+++ floodlight/normal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-NORMAL='{"switch": "00:01:00:9c:02:b1:ff:c0", "name":"normal-drain", "cookie":"0", "priority":"0", "active":"true", "actions":"output=normal"}' 
+NORMAL='{"switch": "'$1'", "name":"normal", "cookie":"0", "priority":"0", "active":"true", "actions":"output=normal"}' 
 echo $NORMAL
 curl -d "$NORMAL" http://127.0.0.1:8080/wm/staticflowentrypusher/json
