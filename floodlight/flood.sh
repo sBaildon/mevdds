33faa6c99c19ec392d23109ccad34627100948db
David Butterworth <d.butterworth@lancaster.ac.uk>
2014-09-11 11:22:32 +0100

    Updated naming of the rule in flood.sh for easier reading

--- floodlight/flood.sh
+++ floodlight/flood.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-FLOOD='{"switch": "'$1'", "name":"normal-'$1'", "cookie":"0", "priority":"0", "active":"true", "actions":"output=flood"}' 
+FLOOD='{"switch": "'$1'", "name":"flood-'$1'", "cookie":"0", "priority":"0", "active":"true", "actions":"output=flood"}' 
 echo $FLOOD
 curl -d "$FLOOD" http://127.0.0.1:8080/wm/staticflowentrypusher/json
