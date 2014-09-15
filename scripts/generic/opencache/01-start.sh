5e976b08216ef72a767e09901ac38aa1bdc9bdff
David Butterworth <d.butterworth@lancaster.ac.uk>
2014-09-15 16:20:05 +0100

    Fixed MongoDB issue + condensed opencache scripts

--- scripts/generic/opencache/01-start.sh
+++
@@ -1,8 +0,0 @@
-# Update repositories
-#apt-get update >> /dev/null
-
-# Get everything we need from repositories
-echo "[install mongodb]" | tee -a /var/log/mevdds/opencache.log
-apt-get -y install mongodb >> /var/log/mevdds/opencache.log
-
-cd ~
