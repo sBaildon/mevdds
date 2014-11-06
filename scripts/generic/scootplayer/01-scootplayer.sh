6934e28b3e6d48fb1aa1f19a780d23cda2b3a687
Sean Baildon <sean@sbaildon.com>
2014-11-06 15:40:04 +0000

    New install location
    install into the home directory properly

--- scripts/generic/scootplayer/01-scootplayer.sh
+++ scripts/generic/scootplayer/01-scootplayer.sh
@@ -1,6 +1,6 @@
 echo '[Cloning scootplayer]' | tee -a /var/log/mevdds/scootplayer.log
-git clone https://github.com/broadbent/scootplayer.git ~ >> /var/log/mevdds/scootplayer.log
+git clone https://github.com/broadbent/scootplayer.git ~/scootplayer >> /var/log/mevdds/scootplayer.log
 cd ~/scootplayer/
 
-ech '[Installing scootplayer requirements]' | tee -a /var/log/mevdds/scootplayer.log
+echo '[Installing scootplayer requirements]' | tee -a /var/log/mevdds/scootplayer.log
 pip install -r requirements.txt >> /var/log/mevdds/scootplayer.log
