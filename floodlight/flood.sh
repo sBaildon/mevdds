a3ec74bd6241456ba6a0e915f104d517db08c5e4
Sean Baildon <sean@baildon.co>
2015-01-24 10:33:00 +0000

    Remove floodlight

--- floodlight/flood.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-FLOOD='{"switch": "'$1'", "name":"normal-'$1'", "cookie":"0", "priority":"0", "active":"true", "actions":"output=flood"}' 
-echo $FLOOD
-curl -d "$FLOOD" http://127.0.0.1:8080/wm/staticflowentrypusher/json
--- floodlight/normal.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-NORMAL='{"switch": "'$1'", "name":"normal-'$1'", "cookie":"0", "priority":"0", "active":"true", "actions":"output=normal"}' 
-echo $NORMAL
-curl -d "$NORMAL" http://127.0.0.1:8080/wm/staticflowentrypusher/json
